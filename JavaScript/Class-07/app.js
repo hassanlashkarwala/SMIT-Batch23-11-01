4a4016fb3722e9dcf4f01d5254e48ccf87258f73
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-29 12:37:10 +0500

    covered chapter 12 & 13 JS

---
+++ JavaScript/Class-07/app.js
@@ -0,0 +1,121 @@
+// Revision Class-06
+// let marks = 40;
+// if(marks >= 50){
+// console.log("Pass");
+// }else{
+//     console.log("Fail");
+// }
+
+// let password = "12345";
+// if(password == 12345){
+// console.log("Login");
+// }else{
+//     console.log("In correct password");
+// }
+
+// Start new topics
+// Today we learn chapter 12 & 13 :)
+
+// Only use if condition first!
+
+// let isRaining = true;
+// if (isRaining) {
+//     console.log("Take Umbrella");
+// }
+
+// let isRaining = false;
+// if (isRaining) {
+//     console.log("Take Umbrella");
+// }
+
+// If else
+// let isRaining = false;
+// if (isRaining) {
+//     console.log("Take Umbrella");
+// } else {
+//     console.log("Enjoy Weather");
+// }
+
+// Now used multiple conditions with the help of if else, else if!
+
+// let marks = 85;
+// if (marks >= 90) {
+//     console.log("A");
+// }
+// else if (marks >= 80) {
+//     console.log("B");
+// }
+// else if (marks >= 70) {
+//     console.log("C");
+// }
+// else {
+//     console.log("Fail");
+// }
+
+// Ex 2 of multiple conditions
+// let weekDays = "Saturday";
+// if(weekDays === "Monday" ){ 
+//    console.log("Today is Monday");
+// }else if(weekDays === "Tuesday"){
+//     console.log("Today is Tuesday");   
+// }else if(weekDays === "Friday"){
+//     console.log("Today is Friday");   
+// }else{
+//     console.log("Does'nt matched");
+// }
+
+// Chapter 13 Logical Operators.
+// OR Operator 
+// ||
+
+// AND Operator
+//  &&
+
+// Ex of OR Operator
+// let age = 20;
+// let parent = false;
+// if (age >= 18 || parent) {
+//     console.log("Allowed");
+// }else{
+//     console.log("Not Allowed");
+// }
+
+// let age = 20;
+// let parent = false;
+
+// if (age >= 18 || parent) {
+//     console.log("Allowed");
+// }
+
+// Ex of && Operators
+let age = 22;
+let cnic = false;
+if (age >= 18 && cnic) {
+    console.log("Eligible");
+}else{
+    console.log("Not Eligible");
+}
+
+// Flag naming rule
+// isLoggedIn
+// isAdmin
+// isRaining
+// hasPermission
+// hasDiscount
+// canVote
+
+// EX
+// let isLoggedIn = true;
+// if (isLoggedIn) {
+//     console.log("Welcome");
+// }
+// else {
+//     console.log("Login First");
+// }
+
+// HomeWork! Create Flags
+// isStudent
+// isTeacher
+// isMarried
+// isOnline
+// hasLaptop
