632ab3c0a74e093bba45a1b06ec21d05028d3a14
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-31 15:25:00 +0500

    covered chapter 14, nested if

---
+++ JavaScript/Class-08/app.js
@@ -0,0 +1,99 @@
+// Nested If ...
+
+
+// Syntax If statement
+// if(condition){
+// console.log();
+
+// if(condition){
+//     console.log();
+// }
+// };
+
+
+// EX: 01
+// let marks = 70;
+// let interview = false;
+// if (marks >= 50) {
+// console.log("You passed the exam.");
+
+//     if (interview) {
+//         console.log("Admission Confirmed");
+//     }
+// };
+
+
+
+// EX: 02
+// let age = 10;
+// let hasCNIC = true;
+// if (age >= 18) {
+//     console.log("Age Requirement Completed");
+
+//     if (hasCNIC) {
+//         console.log("Driving License Approved");
+//     }
+// };
+
+
+
+// EX: 03
+// let feePaid = true;
+// let attendance = 85;
+// if (feePaid) {
+//   console.log("Fee Verified");
+
+//   if (attendance >= 80) {
+//     console.log("Eligible for Certificate");
+//   }
+// }
+
+// EX: 04
+// let level = 10;
+// let coins = 500;
+// if (level >= 10) {
+//   console.log("Next Mission Unlocked");
+
+//   if (coins >= 500) {
+//     console.log("Special Weapon Unlocked");
+//   }
+// };
+
+
+// let = 18;
+// let = 27;
+// let = 50;
+
+// if(18){
+//     console.log("Baba");
+//     if(27){
+//      console.log("Barey Baba");
+//     }
+//     if(50){
+//         console.log("Bhudda Baba");
+//     };
+// };
+
+// variable banao
+let level = 50;
+if(level == 50){
+    console.log("You are pro");
+
+     if(level > 70){
+        console.log("Legend");
+    };
+    if(level == 10 || level == 50){
+        console.log("This is equal to 50");
+    }
+    if(level != 40){
+        console.log("Not equal to 50!");
+
+        if(level != 30){
+            console.log("Nh bhai level 30 ke barabar bh nh hai!");
+        }
+    }
+    if(level <= 50){
+        console.log("Need Practice");
+    }
+ 
+};
