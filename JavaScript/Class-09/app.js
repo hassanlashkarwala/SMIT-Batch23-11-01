42dbc318e7907cf4f7a28673646ac1e56d98b1e4
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-08-03 12:44:39 +0500

    Revision previous topics & Covered chapter 15 Array

---
+++ JavaScript/Class-09/app.js
@@ -0,0 +1,76 @@
+// Revision Topics
+// Revision legal name or illegal name
+// let name1 = "Sufyan"
+// console.log(name);
+
+// let 1name = "Asif"
+// console.log(1name);
+
+// Revision familiar operator
+// let sum = 5+8 
+// console.log(sum);
+
+// let sum2 = 7*8
+// console.log(sum2);
+ 
+// Unfamiliar Operator revision
+
+// let number = 15;
+// let number2 = number++;
+
+// console.log(number2);
+// console.log(++number2);
+
+// Pre & post decrement
+// let num = 20;
+
+// console.log(--num);
+// console.log(num--);
+
+
+// Follow Bodmas Rule :)
+    // let a=3;
+    // let b=7;
+    // console.log((a+b)+a*b);
+
+// concat the string!
+
+// let name= "hasnain";
+// let city="karachi";
+// console.log(name+" "+city)
+
+// if statement!
+// let age = 18;
+// if(age == 18){
+//     console.log("you are adult")
+// }else{
+//     console.log("Muteeb");
+// }
+
+// Today Class: 03-08-2026
+//  Chapter 15: Array
+
+// let student1 = "Faizan"
+// let student2 = "Shaheer"
+// let student3 = "Mustufa"
+// let student4 = "Muteeb"
+// let student5 = "Waseem"
+
+// console.log(student5);
+// console.log(student3);
+// console.log(student2);
+// console.log(student4);
+// console.log(student1);
+
+// Syntax of array
+// Length start 1
+// Index number start 0
+let students = ["Hassan", "Bilal", "Owais", "Ahmed", "Haroon"];
+console.log(students);
+// console.log(students[1]);  // specific value!
+// console.log(students[6]); // undefined bcz index no 6 does'nt exist
+
+// How to update the value of array?
+students[4] = "Muteeb";
+students[2] = "Talha";
+console.log(students);
