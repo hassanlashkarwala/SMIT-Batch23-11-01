23098a2a5902a7bad1e368c86007599c3bb263b7
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-08-05 12:41:26 +0500

    Covered chapter 16 & 17, adding or removing element JS

---
+++ JavaScript/Class-10/app.js
@@ -0,0 +1,46 @@
+// Revision Last Class
+// let fruits = ["apple", "mango", "banana"];
+// console.log(fruits);
+// console.log(fruits[1], fruits[2]);
+
+// Chapter 16 & 17 !
+// let pets = [];
+// console.log(pets);
+
+// pets[0] = "dog";
+// pets[1] = "cat";
+// pets[2] = "bird";
+// console.log(pets);
+
+// push method: Adding the last element of an array 
+// pets.push("elephant", "monkey")
+// console.log(pets);
+
+// Pop method: Removing the last element of an array 
+// pets.pop();
+// console.log(pets);
+
+// Shift method: Removing the first element of an array 
+// pets.shift();
+// console.log(pets);
+
+// unshift method: Adding the first element of an array 
+// pets.unshift("fish");
+// console.log(pets);
+// pets.unshift("cow")
+// console.log(pets);
+
+// let students = ["Waseem", "Hanzala", "Aroosh", "Saeed"];
+// console.log(students);
+
+// students.splice(1, 3, "Mustufa", "Anas");
+// console.log(students);
+
+// students.splice(1, 0, "Mustufa", "Anas");
+// console.log(students);
+
+let cars = ["Yaris", "Corolla", "Honda", "Mehran", "Alto"];
+console.log(cars);
+
+let updatedCars = cars.slice(1, 5);
+console.log(updatedCars);
