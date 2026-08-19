3c5e2f1b2fa24824eec7ce2b817103cb497fbc6d
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-08-19 13:21:08 +0500

    chap 23, 24 & 25

---
+++ JavaScript/Class-15/app.js
@@ -0,0 +1,28 @@
+// Chapter 23 & 24 & 25
+
+// indexOf()
+// lastIndexOf()
+// replace()
+// charAt()
+
+let name = "Hassan";
+console.log(name.indexOf("n"));
+
+let message = "Hey, how are you?"
+console.log(message.lastIndexOf("o"));
+
+let bike = "HONDA";
+console.log(bike.indexOf("N"));
+// indexOf find the index number with the help of your given value
+console.log(bike.lastIndexOf("A"));
+
+let text = "Hello, World War II";
+let newText = text.replace("World War II", "How are you?");
+console.log(text);
+console.log(text, newText);
+console.log(newText);
+// replace method replace the words which word you want!
+
+let firstName = "Muhammad"
+console.log(firstName.charAt(4));
+// charAt method find the value with the help of index number.
