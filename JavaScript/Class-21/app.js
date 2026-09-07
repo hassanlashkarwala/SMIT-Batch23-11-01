76ea13e523e34240b1104fdfc378efe5d3b09f5d
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-09-07 13:14:16 +0500

    revision class

---
+++ JavaScript/Class-21/app.js
@@ -0,0 +1,56 @@
+// function myFunction(){
+//     console.log("My function is running!");
+// }
+// myFunction();
+
+// Data ko pass aur received krne ke liye ye do cheze hoti hen!
+// Parameters
+// Arguments
+
+// function myFunction(paramater){
+//   -----> (in function body) 
+// }
+// myFunction(argument)
+
+// let a = "Hassan";
+// let b = "Owais";
+// console.log(a + " " + b);
+
+
+// Sum 2 numbers 
+// function myFunction(num1, num2){
+// console.log(num1 + num2);
+// }
+// myFunction(3, 5);
+
+// concat two strings 
+// function myFunction(firstName, lastName){
+//     console.log(firstName + lastName);
+// }
+// myFunction("Hassan", "Owais")
+
+// used return keyword, achieved data from outside the function
+// function myFunction(num1, num2){
+//  return num1 + num2;
+// }
+// let result = myFunction(2, 4);
+// console.log(result);
+
+
+// Global Varible
+// let globalVariable = "Hello World";
+
+// function callMyFunction(){
+//     console.log(globalVariable);
+// }
+// console.log(globalVariable);
+// callMyFunction()
+
+
+// Local Variable
+function callMyFunction(){
+    let localVarible = "Hey Local Varible!!";
+    console.log(localVarible);
+}
+callMyFunction()
+console.log(localVarible);
